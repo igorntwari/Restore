edde175c71f30491dab6c57fa6c0b33d9af6b417
igorntwari <igorntwari28@gmail.com>
2025-08-19 12:28:33 +0200

    feat: add the seeding datat before populate

---
+++ API/Data/Migrations/20250819102125_InitialCreate01.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PictureUrl",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "PictureUrl",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
